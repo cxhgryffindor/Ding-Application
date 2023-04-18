40e4f8577e7fa2efe006e4da2e392f7fd636ba2f
rjing01 <94167208+rjing01@users.noreply.github.com>
2023-04-19 00:12:11 +0800

    Add files via upload

--- app/src/main/java/utar/edu/my/registerlogin/MainActivity.java
+++ app/src/main/java/utar/edu/my/registerlogin/MainActivity.java
@@ -1,125 +1,28 @@
-package utar.edu.my.registerlogin;
+package my.edu.utar.loginregisterpage;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.firestore.DocumentChange;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
-import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import utar.edu.my.registerlogin.Adapter.ToDoAdapter;
-import utar.edu.my.registerlogin.Model.ToDoModel;
-
-public class MainActivity extends AppCompatActivity implements OnDialogCloseListener{
-
-
-    private RecyclerView recyclerView;
-    private FloatingActionButton mFab;
-    private FirebaseFirestore firestore;
-    private ToDoAdapter adapter;
-    private List<ToDoModel> mList;
-    private Query query;
-    private ListenerRegistration listenerRegistration;
+public class MainActivity extends AppCompatActivity {
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        recyclerView = findViewById(R.id.recyclerView);
-        mFab = findViewById(R.id.floatingActionButton);
-        firestore = FirebaseFirestore.getInstance();
+        tvSignUp = findViewById(R.id.tvSignUp);
 
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-        mFab.setOnClickListener(new View.OnClickListener() {
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                addNewTask.newInstance().show(getSupportFragmentManager() , addNewTask.TAG);
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+                return;
             }
         });
-
-        mList = new ArrayList<>();
-        adapter = new ToDoAdapter(MainActivity.this, mList);
-
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new TouchHelper(adapter));
-        itemTouchHelper.attachToRecyclerView(recyclerView);
-        showData();
-        recyclerView.setAdapter(adapter);
-
-        //Bottom menu
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomViewNavigation);
-        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
-
-        bottomNavigationView.setOnItemSelectedListener(item -> {
-            switch (item.getItemId()){
-                case R.id.bottom_home:
-                    return true;
-                case R.id.bottom_focus:
-                    startActivity(new Intent(getApplicationContext(),focus_page.class));
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-                case R.id.bottom_profile:
-                    startActivity(new Intent(getApplicationContext(),StatisticActivity.class));
-                    overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
-                    finish();
-                    return true;
-            }
-            return false;
-        });
-
     }
 
-    private void showData(){
-        query = firestore.collection("task").orderBy("time" , Query.Direction.DESCENDING);
-
-        listenerRegistration = query.addSnapshotListener(new EventListener<QuerySnapshot>() {
-                    @Override
-                     public void onEvent(QuerySnapshot value, FirebaseFirestoreException error) {
-                        for (DocumentChange documentChange : value.getDocumentChanges()){
-                             if (documentChange.getType() == DocumentChange.Type.ADDED){
-                                String id = documentChange.getDocument().getId();
-                                ToDoModel toDoModel = documentChange.getDocument().toObject(ToDoModel.class).withId(id);
-
-                                mList.add(toDoModel);
-                                adapter.notifyDataSetChanged();
-                             }
-                        }
-                        listenerRegistration.remove();
-
-                     }
-        });
-    }
-
-    @Override
-    public void onDialogClose(DialogInterface dialogInterface) {
-        mList.clear();
-        showData();
-        adapter.notifyDataSetChanged();
-    }
-
-    public void openFocusPage(){
-        final Intent intent = new Intent(this, focus_page.class);
-        startActivity(intent);
-    }
 }
